5ac8c6edbecaed905598b350d4e7c02ea495492f
amiltner7 <98353048+amiltner7@users.noreply.github.com>
2024-05-29 08:58:42 -0500

    Add files via upload

---
+++ finalproject/config/config.php
@@ -0,0 +1,9 @@
+<?php
+	session_start();
+
+	// Define constants for everything we need to open a mysql connection - will use this in all of my php files
+	define('DB_HOST', '303.itpwebdev.com');
+	define('DB_USER', 'amiltner_db_user');
+	define('DB_PASS', 'Cubs-2022!');
+	define('DB_NAME', 'amiltner_mlb_stats');
+?>
